9dd361b4710332d7df595709aff7ed681003f4b7
Chris Smothers <chris.smothers@gmail.com>
2012-02-23 09:57:42 -0500

    switch to db session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Ananta::Application.config.session_store :cookie_store, key: '_ananta_session'
+# Ananta::Application.config.session_store :cookie_store, key: '_ananta_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Ananta::Application.config.session_store :active_record_store
+Ananta::Application.config.session_store :active_record_store
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120223140629) do
+ActiveRecord::Schema.define(:version => 20120223145631) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -23,5 +23,15 @@ ActiveRecord::Schema.define(:version => 20120223140629) do
 
   add_index "projects", ["name"], :name => "index_projects_on_name"
   add_index "projects", ["state"], :name => "index_projects_on_state"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
