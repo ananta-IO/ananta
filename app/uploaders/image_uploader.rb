20b92378a57185a07965686bae03dc23d6094601
Chris Smothers <chris.smothers@gmail.com>
2012-03-30 14:23:57 -0400

    very basic broken json

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/images/#{model.image_type.pluralize}/#{model.class.to_s.underscore}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore.pluralize}/#{model.image_type.pluralize}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
