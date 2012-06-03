07a05aebe6cc1556b6ae5dde1b9e37db5ce2c9d8
Chris Smothers <chris.smothers@gmail.com>
2012-06-02 23:17:09 -0400

    show flashes on js login

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,11 +15,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.save
       session.delete(:facebook_user_attributes) if session[:facebook_user_attributes]
       if resource.active_for_authentication?
-        set_flash_message(:notice, :signed_up) if is_navigational_format?
+        set_flash_message(:notice, :signed_up)
         sign_in(resource_name, resource)
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
-        set_flash_message(:notice, :"signed_up_but_#{resource.inactive_message}") if is_navigational_format?
+        set_flash_message(:notice, :"signed_up_but_#{resource.inactive_message}")
         expire_session_data_after_sign_in!
         respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
 	def create
 		resource = warden.authenticate!(auth_options)
-		set_flash_message(:notice, :signed_in) if is_navigational_format?
+		set_flash_message(:notice, :signed_in)
 		sign_in(resource_name, resource)
 		@user = resource
 		respond_with @user, :location => after_sign_in_path_for(@user)
