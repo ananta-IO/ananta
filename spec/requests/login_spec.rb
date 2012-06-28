0caf22ffc34699b693f3b185c46cc79579146edc
Chris Smothers <chris.smothers@gmail.com>
2012-06-28 03:53:24 -0400

    use unique username and email

--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -13,9 +13,9 @@ describe 'Login', request: true, slow: true, js: true do
 				visit root_path
 				find(:css, 'a.login').click
 				click_link "Don't have a Facebook account?"
-				fill_in "email", with: 'ananta@ananta.io'
+				fill_in "email", with: 'nue-user@ananta.io'
 				fill_in "password", with: '12345678'
-				fill_in "username", with: 'ananta'
+				fill_in "username", with: 'nue-user'
 				fill_in "project-name", with: 'ananta IO'
 				click_button "Register"
 				pending 'stub geocode and gravatar'
