9a5e49e32b88b9bd5e96e0aa473996f6c59f5142
Chris Smothers <chris.smothers@gmail.com>
2012-09-16 09:07:45 -0400

    link to mentions and tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+	include Twitter::Extractor
+
 	def tb_alert_name name
 		tb_names = {'notice' => 'success'}
 		tb_names[name.to_s] || name.to_s
@@ -15,5 +17,22 @@ module ApplicationHelper
 	def markdown(text)
 		text ||= '' # markdown barfs on nil
 		raw $markdown.render(text)
+	end
+
+	def tagged_markdown(text)
+		text ||= ''
+
+		usernames = extract_mentioned_screen_names(text).uniq
+		tags = extract_hashtags(text).uniq
+
+		usernames.each { |u, i| text = text.gsub(/@#{u}/, "ausrname#{i}") }
+		tags.each { |t, i| text = text.gsub(/##{t}/, "ahshtag#{i}") }
+
+		text = markdown text
+
+		usernames.each { |u, i| text = text.gsub(/ausrname#{i}/, "<a href='#{root_url}#{u}'>@#{u}</a>") }
+		tags.each { |t, i| text = text.gsub(/ahshtag#{i}/, "<a href='#{root_url}tags/#{t}'>##{t}</a>") }
+
+		raw text
 	end
 end
